0f56ba6f602f8fbafd5adc0e5b8f26b84feeaa04
gilmarferrini <ferrinigilmar@gmail.com>
2021-09-18 12:06:57 -0300

    adicionado rota para deletar todo

--- src/index.js
+++ src/index.js
@@ -96,7 +96,17 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const todo = user.todos.find((item) => item.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found!' })
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.status(200).send();
 });
 
 module.exports = app;
